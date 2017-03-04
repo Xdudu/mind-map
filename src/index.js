c0acba49e818f4b3e0bcd2669dc6b0c5f365e7d8
Xdudu <liuyuewuyu@yeah.net>
2017-03-04 15:14:21 +0800

    Featrue: scroll to view

--- src/index.js
+++ src/index.js
@@ -7,3 +7,13 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+document.onreadystatechange = function () {
+  if (document.readyState === "complete") {
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+    var winScrollX = 2333 - windowWidth / 4;
+    var winScrollY = 2333 - windowHeight / 3;
+    window.scroll(winScrollX, winScrollY);
+  }
+}
